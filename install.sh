b47be42a63bf7dea631c161194573442dcf092ea
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-12-10 13:55:23 +0200

    chore: suppress error messages from the migrate:fresh command

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ if [ -f ./composer.lock ]; then
     php artisan ide-helper:models -N
 
     touch ./database/database.sqlite
-    php artisan migrate:fresh --seed || php artisan migrate --seed
+    php artisan migrate:fresh --seed 2>/dev/null || php artisan migrate --seed
 fi
 
 pnpm install
