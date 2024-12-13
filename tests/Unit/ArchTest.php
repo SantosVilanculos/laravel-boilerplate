b8b3fefba7c807e0e0a1e00bbadf82b6bd405166
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-12-13 20:04:51 +0200

    test: use custom strict rules

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -1,5 +1,14 @@
 <?php
 
-arch()->preset()->php();
-arch()->preset()->security();
-arch()->preset()->strict();
+describe('arch', function () {
+    arch()->preset()->php();
+    arch()->preset()->security();
+
+    arch()->expect('App')->toUseStrictTypes();
+    arch()->expect('Database\Factories')->toUseStrictTypes();
+    arch()->expect('Database\Seeders')->toUseStrictTypes();
+
+    arch()->expect('App')->classes()->not->toBeFinal();
+    arch()->expect('Database\Factories')->classes()->not->toBeFinal();
+    arch()->expect('Database\Seeders')->classes()->not->toBeFinal();
+});
