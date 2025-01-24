66b3ab475c555cffe1fe023466b4a5928d379893
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-25 01:47:59 +0200

    refactor: use App facade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ namespace App\Providers;
 use Carbon\CarbonImmutable;
 // use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -27,12 +28,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        DB::prohibitDestructiveCommands($this->app->isProduction());
+        DB::prohibitDestructiveCommands(App::isProduction());
 
-        Model::shouldBeStrict(! $this->app->isProduction());
+        Model::shouldBeStrict(! App::isProduction());
         Model::unguard();
 
-        Password::defaults(fn () => $this->app->isProduction() ? Password::min(8)->max(24)->uncompromised() : null);
+        Password::defaults(fn () => App::isProduction() ? Password::min(8)->max(24)->uncompromised() : null);
 
         Date::use(CarbonImmutable::class);
 
