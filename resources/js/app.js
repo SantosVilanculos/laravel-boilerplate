af656985d08fc83fef4f6911f5ef38c188a5d427
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-25 01:52:40 +0200

    feat(Swal): install sweetalert2 and add Swal listener on livewire init

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-import './bootstrap';
+import './bootstrap.js';
+import Swal from 'sweetalert2';
+
+document.addEventListener('livewire:init', () => {
+    Livewire.on('Swal', ({ titleText = undefined, text = undefined, icon = undefined }) => {
+        // titleText?: string | undefined
+        // text?: string | undefined
+        // icon?: string<'success' | 'error' | 'warning' | 'info' | 'question'> | undefined
+
+        Swal.fire({
+            titleText,
+            text,
+            icon,
+            toast: true,
+            position: 'top-end',
+            timer: 3_000,
+            timerProgressBar: true,
+            showConfirmButton: false,
+            didOpen: popup => {
+                popup.onmouseenter = Swal.stopTimer;
+                popup.onmouseleave = Swal.resumeTimer;
+            }
+        });
+    });
+});
