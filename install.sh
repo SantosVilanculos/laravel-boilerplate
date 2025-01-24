b5e739918af5240b9dcec94a41631fbcd342fa4f
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-01-25 01:55:28 +0200

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,17 @@
 
 cp ./.env.example ./.env
 
-composer install
+composer install --no-interaction
 
 if [ -f ./composer.lock ]; then
     php artisan key:generate
     php artisan storage:link
+
     php artisan ide-helper:generate
     php artisan ide-helper:models -N
 
     touch ./database/database.sqlite
-    php artisan migrate:fresh --seed 2>/dev/null || php artisan migrate --seed
+    php artisan migrate:fresh --seed || php artisan migrate --seed
 fi
 
 pnpm install
